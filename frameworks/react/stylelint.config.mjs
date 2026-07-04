75453768b00dcf1124f2a2aadd54ed1dca179043
akky-xxxx <114.s.aki@gmail.com>
2026-07-04 20:52:52 +0900

    chore(react): change selector-class-pattern to camel case

--- frameworks/react/stylelint.config.mjs
+++ frameworks/react/stylelint.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('stylelint').Config} */
 const config = {
-  extends: ["stylelint-config-standard", "stylelint-config-css-modules"]
+  extends: ["stylelint-config-standard", "stylelint-config-css-modules"],
+  rules: {
+    "selector-class-pattern": "^[a-z]+[a-zA-Z0-9]*$"
+  }
 }
 export default config
