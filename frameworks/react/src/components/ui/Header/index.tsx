82700c13bcb0a163cf3087e7d06a0e43f913fd73
akky-xxxx <114.s.aki@gmail.com>
2026-07-04 19:19:37 +0900

    feat(react): implement Header as temporary

--- frameworks/react/src/components/ui/Header/index.tsx
+++ frameworks/react/src/components/ui/Header/index.tsx
@@ -1,5 +1,39 @@
+import Link from "next/link"
+import styles from "./index.module.css"
+
 import type { FC } from "react"
 
+const NAVIGATION_DATA = [
+  {
+    label: "Top",
+    href: "/",
+  },
+  {
+    label: "Calendar",
+    href: "/calendar",
+  },
+  {
+    label: "Create schedule",
+    href: "/createSchedule",
+  },
+] as const
+
 export const Header: FC = () => {
-  return <header>header</header>
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul className={styles.ul}>
+          {NAVIGATION_DATA.map((navigation) => {
+            const { href, label } = navigation
+
+            return (
+              <li key={href}>
+                <Link href={href}>{label}</Link>
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
+    </header>
+  )
 }
