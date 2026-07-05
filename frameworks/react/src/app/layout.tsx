b50d81d95b4fe12fa293cf9ba3d3486e20a55da3
akky-xxxx <114.s.aki@gmail.com>
2026-07-05 13:45:39 +0900

    feat(react): adjust global, reset css

--- frameworks/react/src/app/layout.tsx
+++ frameworks/react/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
+import "./reset.css"
 import { MainLayout } from "@/components/layouts/MainLayout"
 import { Header } from "@/components/ui/Header"
 import { FcWithChildren } from "@/types/FcWithChildren"
