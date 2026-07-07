07ed4c59ae814cba0139bfe99ddce169c9e00332
akky-xxxx <114.s.aki@gmail.com>
2026-07-07 23:28:39 +0900

    refactor(react): stop unnecessary object

--- frameworks/react/src/components/pages/Calendar/components/features/CalendarBody/index.tsx
+++ frameworks/react/src/components/pages/Calendar/components/features/CalendarBody/index.tsx
@@ -26,17 +26,7 @@ const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
 export const CalendarBody: FC<Props> = (props) => {
   const { currentMonth, currentYear, taskList } = props
-  const dates = getDates(currentYear, currentMonth).map((date) => {
-    return {
-      date: date.toString(),
-      taskList: taskList.reduce<string[]>((previousValue, currentValue) => {
-        if (isSameDay(new Date(currentValue.date), date)) {
-          previousValue.push(currentValue.task);
-        }
-        return previousValue;
-      }, [] satisfies string[]),
-    }
-  })
+  const dates = getDates(currentYear, currentMonth)
 
   return (
     <div className={styles.root}>
@@ -50,16 +40,22 @@ export const CalendarBody: FC<Props> = (props) => {
         {DAYS.map((day) => (
           <DayCell key={day} day={day} />
         ))}
-        {dates.map((datum) => {
-          const { date, taskList } = datum
-          const isCurrentMonth = new Date(date).getMonth() + 1 === currentMonth
+        {dates.map((date) => {
+          const isCurrentMonth = date.getMonth() + 1 === currentMonth
+          const stringDate = date.toString()
+          const targetTaskList = taskList.reduce<string[]>((previousValue, task) => {
+            if (isSameDay(task.date, date)) {
+              previousValue.push(task.task)
+            }
+            return previousValue
+          }, [])
 
           return (
-            <li key={date}>
+            <li key={stringDate}>
               <CalendarCell
-                today={date}
+                today={stringDate}
                 isCurrentMonth={isCurrentMonth}
-                taskListComponent={<TaskList taskList={taskList} />}
+                taskListComponent={<TaskList taskList={targetTaskList} />}
               />
             </li>
           )
