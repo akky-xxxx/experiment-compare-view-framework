fa700f85803aca40737bc30a558dcce49eb62927
akky-xxxx <114.s.aki@gmail.com>
2026-07-07 21:53:13 +0900

    feat(react): fetch api

--- frameworks/react/src/components/pages/Calendar/components/features/CalendarBody/index.tsx
+++ frameworks/react/src/components/pages/Calendar/components/features/CalendarBody/index.tsx
@@ -1,3 +1,5 @@
+import { isSameDay } from "date-fns"
+
 import { CalendarCell } from "./components/features/CalendarCell"
 import { TaskList } from "./components/features/TaskList"
 import { MonthSelector } from "./components/layouts/MonthSelector"
@@ -9,19 +11,32 @@ import styles from "./index.module.css"
 
 import type { FC } from "react"
 
+type Task = {
+  date: string
+  task: string
+}
+
 type Props = {
   currentMonth: number
   currentYear: number
+  taskList: readonly Task[]
 }
 
 const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
 export const CalendarBody: FC<Props> = (props) => {
-  const { currentMonth, currentYear } = props
-  const dates = getDates(currentYear, currentMonth).map((date) => ({
-    date: date.toString(),
-    taskList: [],
-  }))
+  const { currentMonth, currentYear, taskList } = props
+  const dates = getDates(currentYear, currentMonth).map((date) => {
+    return {
+      date: date.toString(),
+      taskList: taskList.reduce<string[]>((previousValue, currentValue) => {
+        if (isSameDay(new Date(currentValue.date), date)) {
+          previousValue.push(currentValue.task);
+        }
+        return previousValue;
+      }, [] satisfies string[]),
+    }
+  })
 
   return (
     <div className={styles.root}>
--- frameworks/react/src/components/pages/Calendar/index.tsx
+++ frameworks/react/src/components/pages/Calendar/index.tsx
@@ -1,11 +1,42 @@
+"use client"
+
+import { format } from "date-fns"
+import { useState, useEffect } from "react"
+
+import { apiClient } from "@/utilities/apiClient"
+
 import { CalendarBody } from "./components/features/CalendarBody"
 
 import type { FC } from "react"
 
+type Task = {
+  date: string
+  task: string
+}
+
 export const Calendar: FC = () => {
+  const [taskList, setTaskList] = useState<Task[]>([])
+
+  useEffect(() => {
+    apiClient.schedules()
+      .then((response) => {
+        setTaskList(response.data.map((schedule) => {
+          const { date, title: task } = schedule
+
+          return {
+            date: format(new Date(date), "yyyy-MM-dd"),
+            task,
+          }
+        }))
+      })
+      .catch((error: unknown) => {
+        console.error(error)
+      })
+  }, [])
+
   return (
     <div>
-      <CalendarBody currentMonth={7} currentYear={2026} />
+      <CalendarBody currentMonth={7} currentYear={2026} taskList={taskList} />
     </div>
   )
 }
