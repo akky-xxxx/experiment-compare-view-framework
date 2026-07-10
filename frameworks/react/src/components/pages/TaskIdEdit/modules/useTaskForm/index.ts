5db0ad1f5f70074dc90db6b6ce94794813e59688
akky-xxxx <114.s.aki@gmail.com>
2026-07-10 17:58:17 +0900

    refactor(react): cut out to useTaskForm

---
+++ frameworks/react/src/components/pages/TaskIdEdit/modules/useTaskForm/index.ts
@@ -0,0 +1,54 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { apiClient } from "@/utilities/apiClient"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
+import { useForm } from "react-hook-form"
+import * as z from "zod/mini"
+
+import type { ScheduleIdResponse } from "@/utilities/apiClient/types/ScheduleIdResponse"
+
+const FormSchema = z.object({
+  body: z.string().check(z.minLength(1)),
+  date: z.string(),
+  title: z.string().check(z.minLength(1)),
+} satisfies Record<keyof Omit<ScheduleIdResponse, "id">, unknown>)
+
+type Input = Pick<ScheduleIdResponse, "id">
+
+export const useTaskForm = (input: Input) => {
+  const { id } = input
+  const { formState, handleSubmit, register, setValues } = useForm<
+    z.infer<typeof FormSchema>
+  >({
+    resolver: zodResolver(FormSchema),
+  })
+  const router = useRouter()
+  const { isDirty, isValid } = formState
+  const isEnabledSubmit = [isDirty, isValid].every(Boolean)
+  const onSubmit = handleSubmit((data) => {
+    apiClient
+      .putScheduleId({
+        ...data,
+        id,
+      })
+      .then(() => {
+        router.push("/calendar")
+      })
+      .catch((error: unknown) => {
+        console.log(error)
+      })
+  })
+
+  useEffect(() => {
+    apiClient
+      .getScheduleId(id)
+      .then((response) => {
+        setValues(response.data)
+      })
+      .catch((error: unknown) => {
+        console.log(error)
+      })
+  }, [])
+
+  return { isEnabledSubmit, onSubmit, register, setValues }
+}
--- frameworks/react/src/components/pages/TaskIdEdit/useHooks.ts
+++ frameworks/react/src/components/pages/TaskIdEdit/useHooks.ts
@@ -1,53 +1,12 @@
-import { zodResolver } from "@hookform/resolvers/zod"
-import { apiClient } from "@/utilities/apiClient"
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
-import { useForm } from "react-hook-form"
-import * as z from "zod/mini"
+import { useTaskForm } from "./modules/useTaskForm"
 
 import type { ScheduleIdResponse } from "@/utilities/apiClient/types/ScheduleIdResponse"
 
 type Input = Pick<ScheduleIdResponse, "id">
 
-const schema = z.object({
-  body: z.string().check(z.minLength(1)),
-  date: z.string(),
-  title: z.string().check(z.minLength(1)),
-} satisfies Record<keyof Omit<ScheduleIdResponse, "id">, unknown>)
-type Schema = z.infer<typeof schema>
-
 export const useHooks = (input: Input) => {
   const { id } = input
-  const { formState, handleSubmit, register, setValues } = useForm<Schema>({
-    resolver: zodResolver(schema),
-  })
-  const router = useRouter()
-  const { isDirty, isValid } = formState
-  const isEnabledSubmit = [isDirty, isValid].every(Boolean)
-  const onSubmit = handleSubmit((data) => {
-    apiClient
-      .putScheduleId({
-        ...data,
-        id,
-      })
-      .then(() => {
-        router.push("/calendar")
-      })
-      .catch((error: unknown) => {
-        console.log(error)
-      })
-  })
-
-  useEffect(() => {
-    apiClient
-      .getScheduleId(id)
-      .then((response) => {
-        setValues(response.data)
-      })
-      .catch((error: unknown) => {
-        console.log(error)
-      })
-  }, [])
+  const { isEnabledSubmit, onSubmit, register } = useTaskForm({ id })
 
   return { isEnabledSubmit, onSubmit, register }
 }
