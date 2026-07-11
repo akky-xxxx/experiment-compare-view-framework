7e2462a98a05a5b896803e126e8a76e05a57d57e
akky-xxxx <114.s.aki@gmail.com>
2026-07-11 18:10:21 +0900

    feat(angular): use calendar-cell at calendar-body

--- frameworks/angular/src/components/pages/calendar/components/features/calendar-body/calendar-body.ts
+++ frameworks/angular/src/components/pages/calendar/components/features/calendar-body/calendar-body.ts
@@ -1,12 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, computed, input } from "@angular/core";
 import { DayCell } from "./components/ui/day-cell/day-cell";
+import { getDates } from "./modules/getDates";
+import { CalendarCell } from "./components/features/calendar-cell/calendar-cell";
 
 @Component({
   selector: "calendar-body",
   templateUrl: "./calendar-body.html",
   styleUrls: ["./calendar-body.css"],
-  imports: [DayCell],
+  imports: [DayCell, CalendarCell],
 })
 export class CalendarBody {
+  public readonly currentMonth = input.required<number>();
+  public readonly currentYear = input.required<number>();
   protected readonly DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"] as const;
+  public readonly dates = computed(() => getDates(this.currentYear(), this.currentMonth()));
 }
