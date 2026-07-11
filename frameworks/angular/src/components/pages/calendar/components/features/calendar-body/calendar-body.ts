e3055d12f0a6b93eb9cda83b57d3de48abe87a02
akky-xxxx <114.s.aki@gmail.com>
2026-07-11 13:59:48 +0900

    feat(angular): use day-cell in calendar-body

--- frameworks/angular/src/components/pages/calendar/components/features/calendar-body/calendar-body.ts
+++ frameworks/angular/src/components/pages/calendar/components/features/calendar-body/calendar-body.ts
@@ -1,8 +1,12 @@
 import { Component } from "@angular/core";
+import { DayCell } from "./components/ui/day-cell/day-cell";
 
 @Component({
   selector: "calendar-body",
   templateUrl: "./calendar-body.html",
   styleUrls: ["./calendar-body.css"],
+  imports: [DayCell],
 })
-export class Calendar {}
+export class CalendarBody {
+  protected readonly DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"] as const;
+}
