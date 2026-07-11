6a5107542433a686739e3e6a39f80a01a2974da5
akky-xxxx <114.s.aki@gmail.com>
2026-07-11 14:01:03 +0900

    feat(angular): use calendar-body in calendar

--- frameworks/angular/src/components/pages/calendar/calendar.ts
+++ frameworks/angular/src/components/pages/calendar/calendar.ts
@@ -1,7 +1,9 @@
 import { Component } from "@angular/core";
+import { CalendarBody } from "./components/features/calendar-body/calendar-body";
 
 @Component({
   selector: "calendar",
   templateUrl: "./calendar.html",
+  imports: [CalendarBody],
 })
 export class Calendar {}
