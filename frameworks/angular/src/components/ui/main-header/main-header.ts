592b393e9ed26eb20006adc85a354531244b2fd2
akky-xxxx <114.s.aki@gmail.com>
2026-07-11 10:45:10 +0900

    feat(angular): implement main-header as temporary

---
+++ frameworks/angular/src/components/ui/main-header/main-header.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: "main-header",
+  imports: [RouterLink],
+  templateUrl: "./main-header.html",
+  styleUrls: ["./main-header.css"],
+})
+export class MainHeader {}
