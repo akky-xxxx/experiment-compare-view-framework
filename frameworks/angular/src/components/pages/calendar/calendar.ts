35ee5f1a7dbf0d8ff2b9e94940d319ff0ca2a980
akky-xxxx <114.s.aki@gmail.com>
2026-07-11 10:44:19 +0900

    feat(angular): implement page components as temporary

---
+++ frameworks/angular/src/components/pages/calendar/calendar.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "calendar",
+  templateUrl: "./calendar.html",
+})
+export class Calendar {}
---
+++ frameworks/angular/src/components/pages/top/top.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "top",
+  templateUrl: "./top.html",
+})
+export class Top {}
