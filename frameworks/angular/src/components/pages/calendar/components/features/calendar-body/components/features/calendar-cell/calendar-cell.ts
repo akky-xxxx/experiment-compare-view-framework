66b5390028e389197c3b764f4e2ea518dac8513b
akky-xxxx <114.s.aki@gmail.com>
2026-07-11 18:09:44 +0900

    feat(angular): implement calendar-cell

---
+++ frameworks/angular/src/components/pages/calendar/components/features/calendar-body/components/features/calendar-cell/calendar-cell.ts
@@ -0,0 +1,23 @@
+import { Component, computed, input, TemplateRef } from "@angular/core";
+import { NgTemplateOutlet } from "@angular/common";
+import { RouterLink } from "@angular/router";
+
+const getCellColor = (isCurrentMonth: boolean) => {
+  return isCurrentMonth ? 1 : 0.6;
+};
+
+@Component({
+  selector: "calendar-cell",
+  templateUrl: "./calendar-cell.html",
+  styleUrls: ["./calendar-cell.css"],
+  imports: [RouterLink, NgTemplateOutlet],
+})
+export class CalendarCell {
+  public readonly date = input.required<Date>();
+  public readonly isCurrentMonth = input.required<boolean>();
+  public readonly taskListSlot = input.required<TemplateRef<unknown>>();
+  protected currentDate = computed(() => this.date().getDate());
+  protected cellColor = computed(() => ({
+    "--opacity": getCellColor(this.isCurrentMonth()),
+  }));
+}
