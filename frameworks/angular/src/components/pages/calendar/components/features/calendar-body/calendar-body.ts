14414980ee1c11c063d1f92d7235d785d7b5bfa4
akky-xxxx <114.s.aki@gmail.com>
2026-07-11 10:58:00 +0900

    feat(angular): implement calendar-body as temporary

---
+++ frameworks/angular/src/components/pages/calendar/components/features/calendar-body/calendar-body.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "calendar-body",
+  templateUrl: "./calendar-body.html",
+  styleUrls: ["./calendar-body.css"],
+})
+export class Calendar {}
