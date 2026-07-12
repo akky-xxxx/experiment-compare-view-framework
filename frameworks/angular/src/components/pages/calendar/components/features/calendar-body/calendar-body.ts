1aacad4d32720ebaf94f4824302ccc2336be0a07
akky-xxxx <114.s.aki@gmail.com>
2026-07-12 15:26:29 +0900

    feat(angular): use month-selector, sibling-month-button

--- frameworks/angular/src/components/pages/calendar/components/features/calendar-body/calendar-body.ts
+++ frameworks/angular/src/components/pages/calendar/components/features/calendar-body/calendar-body.ts
@@ -1,17 +1,21 @@
-import { Component, computed, input } from "@angular/core";
+import { Component, computed, input, output } from "@angular/core";
 import { DayCell } from "./components/ui/day-cell/day-cell";
 import { getDates } from "./modules/getDates";
 import { CalendarCell } from "./components/features/calendar-cell/calendar-cell";
+import { MonthSelector } from "./components/layouts/month-selector/month-selector";
+import { SiblingMonthButton } from "./components/ui/sibling-month-button/sibling-month-button";
 
 @Component({
   selector: "calendar-body",
   templateUrl: "./calendar-body.html",
   styleUrls: ["./calendar-body.css"],
-  imports: [DayCell, CalendarCell],
+  imports: [DayCell, CalendarCell, MonthSelector, SiblingMonthButton],
 })
 export class CalendarBody {
   public readonly currentMonth = input.required<number>();
   public readonly currentYear = input.required<number>();
+  public readonly handleNextMonthClick = output();
+  public readonly handlePreviousMonthClick = output();
   protected readonly DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"] as const;
   protected readonly dates = computed(() => getDates(this.currentYear(), this.currentMonth()));
 }
