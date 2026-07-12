d3a8db5762d9569b74a76603bb6be338d657915b
akky-xxxx <114.s.aki@gmail.com>
2026-07-12 15:25:59 +0900

    feat(angular): implement month-selector

---
+++ frameworks/angular/src/components/pages/calendar/components/features/calendar-body/components/layouts/month-selector/month-selector.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "month-selector",
+  templateUrl: "./month-selector.html",
+  styleUrls: ["./month-selector.css"],
+})
+export class MonthSelector {}
