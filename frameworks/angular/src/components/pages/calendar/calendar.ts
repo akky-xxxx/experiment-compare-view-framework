cff06230f1cfcebe6990e04bc2e5a772cdba2850
akky-xxxx <114.s.aki@gmail.com>
2026-07-12 15:27:20 +0900

    feat(angular): hands over the required props

--- frameworks/angular/src/components/pages/calendar/calendar.ts
+++ frameworks/angular/src/components/pages/calendar/calendar.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { addMonths } from "date-fns";
+import { Component, computed, signal } from "@angular/core";
 import { CalendarBody } from "./components/features/calendar-body/calendar-body";
 
 @Component({
@@ -6,4 +7,17 @@ import { CalendarBody } from "./components/features/calendar-body/calendar-body"
   templateUrl: "./calendar.html",
   imports: [CalendarBody],
 })
-export class Calendar {}
+export class Calendar {
+  private readonly today = signal(new Date());
+
+  protected readonly currentMonth = computed(() => this.today().getMonth() + 1);
+  protected readonly currentYear = computed(() => this.today().getFullYear());
+
+  protected readonly handleNextMonthClick = () => {
+    this.today.update((current) => addMonths(current, 1));
+  };
+
+  protected readonly handlePreviousMonthClick = () => {
+    this.today.update((current) => addMonths(current, -1));
+  };
+}
